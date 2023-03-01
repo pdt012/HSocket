48008be5a2450a203d05a2239784609b0fb1735f
pdt012 <jxh0615@163.com>
2023-03-01 22:11:15 +0900

    feat(py): add setup.py

---
+++ python/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+NAME = "hsocket"
+VERSION = "1.0.0"
+DESCRIPTION = "hsocket for python"
+AUTHOR = "pdt012"
+AUTHOR_EMAIL = "jxh0615@163.com"
+URL = "https://github.com/pdt012/HSocket"
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+)
