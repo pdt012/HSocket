958e690011628e664c6dc5d610c7c0bf0f56e48a
pdt012 <jxh0615@163.com>
2023-03-06 23:44:26 +0900

    test(cs): add test

---
+++ cs/test/Program.cs
@@ -0,0 +1,14 @@
+﻿using hsocket;
+using System.Text.Json.Nodes;
+
+Message msg = Message.PlainTextMsg(2, "helloworld");
+Console.WriteLine(msg);
+
+
+JsonObject obj = new JsonObject();
+obj.Add("aaa", 12345);
+obj.Add("bbb", "bbbvalue");
+Console.WriteLine(obj.ToJsonString());
+
+msg = Message.JsonMsg(2, obj);
+Console.WriteLine(msg);
